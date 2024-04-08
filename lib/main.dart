aeff8c3b7c9467c0ffebf7b45012953c41e65d37
IGSparkew <alex.robbrecht@sfr.fr>
2024-04-08 16:34:54 +0200

    ajout de personnalisation des composants

--- lib/main.dart
+++ lib/main.dart
@@ -87,28 +87,116 @@ class BodyTabApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return TabBarView(
       children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/campagne_pics.jpg'),
-            Column(
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Text("Paris, France"),
-                    ],
-                  ),
-                  Text("3-8 septembre"),
-                  Text("140€ le nuit"),
-                ]),
-          ],
-        ),
-        Container(child: Text("sur l'eau")),
-        Container(child: Text("avec vue")),
-        Container(child: Text("bord de mer")),
-        Container(child: Text("ski")),
+        DestinationDetails(path: "assets/campagne_pics.jpg", place: 'Paris, France', date: '3-9 avril', price: '120€ nuit'),
+        DestinationDetails(path: "assets/venise.jpg", place: 'Venise, Italie', date: '3-9 avril', price: '120€ nuit'),
+        DestinationDetails(path: "assets/new_york.jpeg", place: 'New York, USA', date: '3-9 avril', price: '200€ nuit'),
+        DestinationDetails(path: "assets/bali.jpg", place: 'Bali, Indonésie', date: '3-9 avril', price: '300€ nuit'),
+        DestinationDetails(path:"assets/chamonix.jpg", place: 'Chamonix, France', date: '3-9 avril', price: '120€ nuit'),
       ],
+    );
+  }
+}
+
+class DestinationDetails extends StatelessWidget {
+  final String path;
+  final String place;
+  final String date;
+  final String price;
+  const DestinationDetails({super.key, required this.path, required this.place, required this.date, required this.price});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [DestinationPhoto(path: path), DestinationInfos(place: place,date: date, price: price,)],
+    );
+  }
+}
+
+
+class DestinationPhoto extends StatelessWidget {
+  final String path;
+  const DestinationPhoto({super.key, required this.path});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: 3.0,
+          child: Image.asset(
+            path,
+            fit: BoxFit.cover,
+            errorBuilder: (context, _, __) =>
+            const Icon(Icons.warning, color: Colors.red),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border),
+          ),
+        ),
+        Positioned(left: 8, top: 8, child: MembersFavorite())
+      ],
+    );
+  }
+}
+
+class MembersFavorite extends StatelessWidget {
+  const MembersFavorite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white70,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.favorite, color: Colors.pink),
+          /*SizedBox(width: 12),*/
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text('Coup de coeur Voyageurs'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class DestinationInfos extends StatelessWidget {
+  final String place;
+  final String date;
+  final String price;
+  DestinationInfos({super.key, required this.place, required this.date, required this.price});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              Text(place),
+              Spacer(),
+              Icon(Icons.star, color: Colors.orange),
+              Text('4.78')
+            ],
+          ),
+          Text(date),
+          Text(price),
+        ],
+      ),
     );
   }
 }
